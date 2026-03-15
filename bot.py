f0b30b126eaea628ad70da906feb8969fc0ebc62
tommik85 <39844035+tommik85@users.noreply.github.com>
2026-03-16 00:15:26 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,38 @@
+import feedparser
+import requests
+import time
+import os
+
+TOKEN = os.getenv("8771208535:AAHFueJ0szulZWaZwz7MikY30LdSxNp9v1Q")
+CHAT_ID = os.getenv("8397011743")
+
+feeds = [
+"https://www.iltalehti.fi/rss/nhl.xml",
+"https://www.is.fi/rss/nhl.xml",
+"https://www.tsn.ca/rss/nhl",
+"https://www.sportsnet.ca/hockey/nhl/feed/"
+]
+
+seen=set()
+
+def send(msg):
+ url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+ requests.post(url,data={"chat_id":CHAT_ID,"text":msg})
+
+while True:
+
+ for feed_url in feeds:
+
+  feed=feedparser.parse(feed_url)
+
+  for entry in feed.entries:
+
+   if entry.link not in seen:
+
+    message=f"🚨 NHL NEWS\n\n{entry.title}\n{entry.link}"
+
+    send(message)
+
+    seen.add(entry.link)
+
+ time.sleep(60)
